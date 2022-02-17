56fbd9c0410c51a17c1af3ba991437e4ec3943a3
unknown <vsameer1408@gmail.com>
2022-02-17 12:32:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log("hello")
+const menuBtn = document.getElementById('navbtn');
+const phoneMenu = document.getElementById('phoneMenu');
+
+const line1 = document.getElementById('l1');
+const line2 = document.getElementById('l2');
+const line3 = document.getElementById('l3');
+
+var phoneMenuOpen = false;
+menuBtn.addEventListener('click',()=>{
+    if(phoneMenuOpen==false){
+    phoneMenu.style.display="block"
+    line1.style.transform="rotate(45deg)"
+    line2.style.transform="rotate(135deg)"
+    line1.style.transition="400ms ease"
+    line2.style.transition="400ms ease"
+    line3.style.display="none"
+    line3.style.transition="400ms ease"
+    phoneMenuOpen = true;
+    
+    }
+    else{
+        phoneMenu.style.display="none"
+        line1.style.transform="rotate(0deg)"
+        line2.style.transform="rotate(0deg)"
+        line1.style.transition="400ms ease"
+        line2.style.transition="400ms ease"
+        line3.style.display="block"
+        line3.style.transition="400ms ease"
+        phoneMenuOpen=false;
+
+    }
+})
