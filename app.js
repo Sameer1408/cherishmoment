c8bfa95ba9be5aee23a2be0d32c3c03e601009b1
Sameer Verma <71380483+Sameer1408@users.noreply.github.com>
2022-02-19 13:40:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log("hello")
 const menuBtn = document.getElementById('navbtn');
 const phoneMenu = document.getElementById('phoneMenu');
+const whatsappicon = document.getElementById('whatsappicon')
+const chatdiv = document.getElementById('chatdiv')
 
 const line1 = document.getElementById('l1');
 const line2 = document.getElementById('l2');
@@ -16,6 +18,7 @@ menuBtn.addEventListener('click', () => {
         line2.style.transition = "400ms ease"
         line3.style.display = "none"
         line3.style.transition = "400ms ease"
+        whatsappicon.style.display = "none"
         phoneMenuOpen = true;
 
     } else {
@@ -26,6 +29,7 @@ menuBtn.addEventListener('click', () => {
         line2.style.transition = "400ms ease"
         line3.style.display = "block"
         line3.style.transition = "400ms ease"
+        whatsappicon.style.display = "block"
         phoneMenuOpen = false;
 
     }
@@ -42,10 +46,53 @@ const servicesDiv = document.getElementById('servicesDiv');
 const cross = document.getElementById('cross');
 const services = document.getElementById('services');
 
-services.addEventListener('click',()=>{
-    servicesDiv.style.display= 'block';
+services.addEventListener('click', () => {
+    servicesDiv.style.display = 'block';
 })
 
-cross.addEventListener('click',()=>{
-    servicesDiv.style.display= 'none';
+cross.addEventListener('click', () => {
+    servicesDiv.style.display = 'none';
 })
+
+
+var visible = false;
+var chats = ['Order Now!', 'How can I help you!', 'Hello!']
+var index = 0;
+
+function chatDivVisibility() {
+    if (visible == false) {
+        chatdiv.style.visibility = 'visible';
+        chatdiv.style.opacity = '1';
+        chatdiv.style.transform = 'visibility 0s linear 0s,opacity 500ms';
+        chatdiv.innerHTML = chats[index]
+        if (index == 2) {
+            index = 0;
+        } else {
+            index = index + 1;
+        }
+
+        visible = true
+    } else {
+        chatdiv.style.visibility = 'hidden';
+        chatdiv.style.opacity = '0';
+        chatdiv.style.transform = 'visibility 0s linear 0s, opacity 500ms';
+        visible = false;
+    }
+}
+
+function showWhatsappIcon() {
+    setTimeout(() => {
+        whatsappicon.style.visibility = "visible";
+        whatsappicon.style.opacity = '1';
+        whatsappicon.style.transform = 'visibility 0s linear 0s,opacity 500ms';
+    }, 2000);
+}
+
+window.onload = function() {
+    showWhatsappIcon();
+    setInterval(chatDivVisibility, 5000);
+};
+
+// whatsappicon.addEventListener('mouseover', () => {
+//     setInterval(chatDivVisibility, 2000)
+// })
